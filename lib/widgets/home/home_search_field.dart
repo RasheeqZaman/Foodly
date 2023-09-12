d0043ef5741ce6f927b0e3d2f3badc7267dfe26e
zaman <zamanrshq@gmail.com>
2023-09-12 15:15:54 +0600

    Add: Home

---
+++ lib/widgets/home/home_search_field.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+import 'package:foodly/utils/constants.dart';
+
+class HomeSearchField extends StatelessWidget {
+  const HomeSearchField({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: const EdgeInsets.all(5.0),
+      padding: const EdgeInsets.symmetric(horizontal: 10.0),
+      decoration: BoxDecoration(
+        color: Constants.colorGrey,
+        borderRadius: BorderRadius.circular(10.0),
+      ),
+      child: Row(
+        children: [
+          Icon(
+            Icons.search,
+            color: Constants.colorGunmetal.withOpacity(0.5),
+          ),
+          Expanded(
+            child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 10.0),
+              child: TextField(
+                decoration: InputDecoration(
+                  border: InputBorder.none,
+                  hintText: 'Search food and restaurants',
+                  hintStyle: Constants.textStyleBody.copyWith(
+                    color: Constants.colorGunmetal.withOpacity(0.5),
+                  ),
+                ),
+                style: Constants.textStyleBody,
+              ),
+            ),
+          ),
+          const Icon(Icons.filter_list_outlined),
+        ],
+      ),
+    );
+  }
+}

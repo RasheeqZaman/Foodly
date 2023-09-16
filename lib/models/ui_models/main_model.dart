84a89dcff04a8b1c97a6c64721924d6cc122dd54
zaman <zamanrshq@gmail.com>
2023-09-16 23:17:46 +0600

    Add: Main Tabs

--- lib/models/ui_models/main_model.dart
+++ lib/models/ui_models/main_model.dart
@@ -2,10 +2,8 @@ import 'package:flutter/material.dart';
 
 class MainModel {
   final PreferredSizeWidget Function() appBar;
-  final Widget tab;
 
   MainModel({
     required this.appBar,
-    required this.tab,
   });
 }
--- lib/view_models/main_view_model.dart
+++ lib/view_models/main_view_model.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/scheduler.dart';
-import 'package:foodly/views/home/home_view.dart';
 import 'package:foodly/widgets/common/app_bar_widget.dart';
 
 import '../models/ui_models/main_model.dart';
@@ -9,39 +8,33 @@ import 'base_view_model.dart';
 class MainViewModel extends BaseViewModel implements TickerProvider {
   MainViewModel({
     required int selectedIndex,
-  }) : _selectedIndex = selectedIndex;
+  });
 
   TabController? _tabController;
 
   TabController? get tabController => _tabController;
 
-  int _selectedIndex = 0;
-
-  int get selectedIndex => _selectedIndex;
+  int get selectedIndex => _tabController?.index ?? 0;
 
   final List<MainModel> _tabs = [
     MainModel(
       appBar: () => const HomeAppBar(),
-      tab: const HomeView(isNearByTab: false),
     ),
     MainModel(
       appBar: () => const HomeAppBar(),
-      tab: const HomeView(isNearByTab: true),
     ),
     MainModel(
       appBar: () => const CartAppBar(),
-      tab: const Center(child: Text('Cart')),
     ),
     MainModel(
       appBar: () => const AccountAppBar(),
-      tab: const Center(child: Text('Account')),
     ),
   ];
 
   PreferredSizeWidget get selectedAppBar =>
-      (_selectedIndex < 0 || _selectedIndex >= _tabs.length)
+      (selectedIndex < 0 || selectedIndex >= _tabs.length)
           ? AppBar(title: const Text('Invalid Tab'))
-          : _tabs[_selectedIndex].appBar();
+          : _tabs[selectedIndex].appBar();
 
   @override
   void disposeViewModel() {}
@@ -53,7 +46,7 @@ class MainViewModel extends BaseViewModel implements TickerProvider {
   }
 
   void onChangeBottomTab(int index) {
-    _selectedIndex = index;
+    _tabController?.animateTo(index);
     notifyListeners();
   }
 
